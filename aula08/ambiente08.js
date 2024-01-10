5876442f76fe8172c4bda6e47595a54ca6b16a45
jotamedeiros <devjoaogui@gmail.com>
2024-01-10 15:09:15 -0300

    aula08 - operadores bitwise

---
+++ aula08/ambiente08.js
@@ -0,0 +1,25 @@
+// Operador '&'
+
+let b1 = 11
+let b2 = 14
+
+let r = b1 & b2
+
+// Operador '|'
+
+let a1 = 11
+let a2 = 14
+
+let resp = a1 | a2
+
+// Operador XOR [^]
+
+let n1 = 10
+
+
+let res = n1 << 1
+
+let resposta = n1 >> 1
+
+console.log(res)
+
