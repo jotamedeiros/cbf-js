cf52f41bb058cb66f7108bdbd0963d40ade22c6f
jotamedeiros <devjoaogui@gmail.com>
2024-01-10 15:49:39 -0300

    aula09 - pré & pós incrementos, inversão e concatenação

---
+++ aula09/ambiente09.js
@@ -0,0 +1,26 @@
+// Operador de pré-incremento
+
+let a = 10
+
+console.log(++a)
+
+// Operador de pós-incremento
+
+let b = 10
+
+console.log(b++)
+
+// Operador de inversão
+
+let n = -10
+let x = -n
+
+console.log(n)
+console.log(x)
+
+// Operador de concatenação
+
+let n1 = 10
+let n2 = 20
+
+console.log(n1 + '' +n2)
