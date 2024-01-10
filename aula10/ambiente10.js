71cd38e2f91abf5dc14bc61b3ebd36fbc02f0737
jotamedeiros <devjoaogui@gmail.com>
2024-01-10 16:22:51 -0300

    aula10 - operadores/operações ternárias

---
+++ aula10/ambiente10.js
@@ -0,0 +1,26 @@
+let num = 10
+
+res = (!(num % 2) ? 'Par' : 'Ímpar')
+
+console.log(res)
+
+// 0 = false
+// 1 = true
+
+// teste lógico ? ifTrue : ifFalse
+
+
+let n1 = 10
+let n2 = 5
+
+resp = (n1 > n2 ? 'Maior' : 'Menor')
+
+console.log(resp)
+
+
+// situação aplicável
+// Banco de dados que retorna se o status do usuário está "A" [ativo] ou "I" [inativo]
+
+let stat = 'A'
+
+res = (stat == 'A' ? 'Ativo' : 'Inativo')
