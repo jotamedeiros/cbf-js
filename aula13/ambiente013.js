8277a922a1dfaab495d3138bcbd464a5cf252432
jotamedeiros <devjoaogui@gmail.com>
2024-01-11 16:29:38 -0300

    aula13 - if / if & else / else if / aninhamento

---
+++ aula13/ambiente013.js
@@ -0,0 +1,10 @@
+let energia = 100
+let clima = 'sol'
+
+if (clima == 'sol' || clima == 'nublado') {
+    console.log('Vou à praia.')
+} else {
+    console.log('Vou ao cinema.')
+}
+
+console.log('Fim do programa')
