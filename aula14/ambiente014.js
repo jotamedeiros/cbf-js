93e38c3f99c79b23a4618c8f26a61f676f969a3d
jotamedeiros <devjoaogui@gmail.com>
2024-01-11 16:55:43 -0300

    aula14 - switch case

---
+++ aula14/ambiente014.js
@@ -0,0 +1,50 @@
+// let colocacao = 5
+
+// switch (colocacao) {
+//     case 1:
+//         console.log('Primeiro colocado')
+//         break
+
+//     case 2:
+//         console.log('Segundo colocado')
+//         break
+    
+//     case 3:
+//         console.log('Terceiro colocado')
+//         break
+
+//     case 4: case 5: case 6:
+//         console.log('Premio de consolação')
+//         break
+    
+//     default:
+//         console.log('Não subiu ao pódio')
+//         break
+// }
+
+let nota = ''
+
+switch (nota) {
+    case 'a':
+        console.log('Parabéns! Você tirou A!')
+        break
+
+    case 'b':
+        console.log('Ótimo! Você tirou B!')
+        break
+
+    case 'c':
+        console.log('Legal! Você tirou C!')
+        break
+
+    case 'd':
+        console.log('Cuidado! Você tirou D!')
+        break
+
+    case 'e': case 'f':
+        console.log('Que pena! Você reprovou!')
+        break
+
+    default:
+        console.log('Você não fez a prova!')
+}
