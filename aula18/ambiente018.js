1b467e05f51568dc507763bba0f65d3cb332b19c
jotamedeiros <devjoaogui@gmail.com>
2024-01-12 16:44:25 -0300

    aula018 - diferença entre while & doWhile

---
+++ aula18/ambiente018.js
@@ -0,0 +1,16 @@
+let n = 10
+
+// while (n < 10) {
+//     console.log('Santos!')
+//     n++
+// }
+
+// console.log('Fim do programa')
+
+
+do {
+    console.log('Santos!')
+    n++
+} while (n < 10)
+
+console.log ('Fim do programa')
