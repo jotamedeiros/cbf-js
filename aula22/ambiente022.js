3eafa296ee456f6f2dc4bc54fa6a3c5b55efb242
jotamedeiros <devjoaogui@gmail.com>
2024-01-15 15:03:18 -0300

    aula22 - funções parametrizadas

---
+++ aula22/ambiente022.js
@@ -0,0 +1,53 @@
+// FUNÇÃO PARAMETRIZADA
+
+// function soma(n1, n2) {
+//     console.log(n1 + n2)
+// }
+
+// soma(10, 11)
+
+
+// FUNÇÃO COM MAIS PARÂMETROS
+
+// function somaMultiplica (n1, n2, n3) {
+//     let soma = n1 + n2
+//     let res = soma * n3
+//     return res
+// }
+
+// console.log(somaMultiplica(10, 2, 3))
+
+
+// FUNÇÃO COM PARÂMETROS 'DEFAULT'
+
+// function soma(n1=0, n2=0) {
+//     return n1 + n2
+// }
+
+// console.log(soma(10, 10))
+
+// FUNÇÃO C/ PARÂMETROS 'DEFAUL' & RETURN
+
+// function soma(n1=0, n2=5) {
+//     let res = n1 + n2
+//     return res
+// }
+
+// let res_soma = soma(5)
+// console.log(res_soma)
+
+
+let valor = 0
+
+console.log(valor)
+
+function add(v) {
+    valor += v
+}
+
+add(10)
+console.log(valor)
+
+add(5)
+console.log(valor)
+
