7641df85ae3159cbb2d37f6e69e1c39795d0f327
jotamedeiros <devjoaogui@gmail.com>
2024-01-19 15:44:43 -0300

    aula039 - relação dos elementos na DOM pt.2

---
+++ aula39/ambiente039.js
@@ -0,0 +1,23 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+const c1_2 = document.querySelector('#c1_2')
+
+console.log(c1_2.parentNode.parentNode.parentNode.children[5])
+
+// console.log(caixa1.hasChildNodes())
+// console.log(btn_c[0].hasChildNodes())
+// console.log(btn_c[0].children)
+
+// if (caixa1.children.length > 0) {
+//     console.log('possui filhos')
+// } else {
+//     console.log('não possui filhos')
+// }
+
+// console.log(caixa1.children.length > 0 ? 'Possui filhos' : 'Não possui filhos')
+
+// console.log(caixa1.firstElementChild)
+
+// caixa1.firstElementChild.innerHTML = 'SANTÁSTICO!'
+// caixa1.children[1].innerHTML = 'LEÃO DO MAR!'
+
