ec5ef7d136179399b05ba701c633f67b012b0e52
jotamedeiros <devjoaogui@gmail.com>
2024-01-24 15:11:27 -0300

    aula051 - iteradores e elementos iteráveis

---
+++ aula51/ambiente051.js
@@ -0,0 +1,26 @@
+// Exemplo de 'iterador' com números
+
+const valores = [10, 8, 9, 2]
+const it_valores = valores[Symbol.iterator]()
+
+console.log(valores)
+console.log(it_valores.next())
+console.log(it_valores.next())
+console.log(it_valores.next())
+console.log(it_valores.next())
+console.log(it_valores.next())
+
+// Exemplo de 'iterador' com string
+
+const texto = 'Youtube'
+const it_texto = texto[Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
