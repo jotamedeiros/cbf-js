efadf2c2bfab79aae16a3e1b8b77bde5fcfa95b9
jotamedeiros <devjoaogui@gmail.com>
2024-01-26 14:30:57 -0300

    aula57 - entendendo orientação à objetos #1

---
+++ aula57/ambiente057.js
@@ -0,0 +1,13 @@
+class Pessoa {
+    constructor(pNome) {
+        this.nome = pNome
+    }
+}
+
+let p1 = new Pessoa('Joao')
+let p2 = new Pessoa('Bruno')
+let p3 = new Pessoa('Cibele')
+
+console.log(p1.nome)
+console.log(p2.nome)
+console.log(p3.nome)
