aec1a5c35c50cd79cc7da4447710f3b3f667c98c
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-06 14:34:04 -0300

    aula77 - aprendendo 'data', 'hora' & seus métodos

---
+++ aula77/ambiente077.js
@@ -0,0 +1,31 @@
+const div_data = document.querySelector('#div_data')
+const div_timer = document.querySelector('#div_timer')
+const data = new Date()
+
+// data
+
+let dia = data.getDate() < 10 ? `0${data.getDate()}`:`${data.getDate()}`
+
+let mes = data.getMonth() < 10 ? `0${data.getMonth()}`:`${data.getMonth()}`
+
+let ano = data.getFullYear()
+
+div_data.innerHTML = `Data: ${dia}/${mes}/${ano}`
+
+// timer
+
+const timer = () => {
+    const data = new Date()
+
+    let hora = data.getHours() < 10 ? `0${data.getHours()}`:`${data.getHours()}`
+
+    let minutos = data.getMinutes() < 10 ? `0${data.getMinutes()}`:`${data.getMinutes()}`
+
+    let segundos = data.getSeconds() < 10 ? `0${data.getSeconds()}`:`${data.getSeconds()}`
+
+    let full_hour = `${hora}:${minutos}:${segundos}`
+
+    div_timer.innerHTML = full_hour
+}
+
+const intervalo = setInterval(timer, 1000)
