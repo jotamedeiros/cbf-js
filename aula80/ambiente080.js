bf3a1da46d4d357f5f130035edb1e00e6b9b613f
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-07 14:24:20 -0300

    aula80 - entendendo a biblioteca 'Math'

---
+++ aula80/ambiente080.js
@@ -0,0 +1,50 @@
+const mat = document.querySelector('#mat')
+
+let n1 = 10
+let n2 = 2
+
+mat.innerHTML = num
+
+/* random() => Retorna um número aleatório entre 0 ~ 1.
+
+const num = Math.random() * 10
+
+mat.innerHTML = num
+
+*/
+
+
+/* floor() => Retorna o maior inteiro acima ou igual o número passado como argumento.
+
+const num = Math.floor(Math.random() * 10)
+
+mat.innerHTML = num
+
+*/
+
+
+/* round()  => Arredonda o valor passado como argumento para o inteiro mais próximo desse valor.
+
+const num = Math.round(Math.random() * 10)
+
+mat.innerHTML = num
+
+*/
+
+
+/* sqrt() => Retorna a raíz quadrada do valor passado como parâmetro.
+
+const num = Math.sqrt(100)
+
+mat.innerHTML = num
+
+*/
+
+
+/* pow() => Retorna a potenciação dos valores passados por parâmetro [x, y]. Onde 'x = base' e 'y = expoente'
+
+const num = Math.pow(10, 2)
+
+mat.innerHTML = num
+
+*/
