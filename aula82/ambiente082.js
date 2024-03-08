af86fcfa884a2219c92d0a9c45aa81bc15df2e47
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-08 10:46:22 -0300

    aula85 - add funcs: posição, desenhar e remover

--- aula82/ambiente082.js
+++ aula82/ambiente082.js
@@ -27,19 +27,34 @@ class Bola {
         this.desenhar()
         this.controle = setInterval(this.controlar, 10)
         this.eu = document.getElementById(this.id)
+        numBola++
+        num_objetos.innerHTML = numBola
     }
 
     minhaPos = () => {
-
+        return this.arrayBolas.indexOf(this)
     }
     
-    desenhar = () => {
-
-    }
-
     remover = () => {
-
+        clearInterval(this.controle)
+        bolas = bolas.filter((el) => {
+            if (el.id != this.id) {
+                return el
+            }
+        })
+        this.eu.remove()
+        numBola--
+        num_objetos.innerHTML = numBola
     }
+
+    desenhar = () => {
+        const div = document.createElement('div')
+        div.setAttribute('id', this.id)
+        div.setAttribute('class', 'bola')
+        div.setAttribute('style', `left: ${this.px}; top: ${this.py}; width: ${this.tam}; height: ${this.tam}; background-color: rgb(${this.r}, ${this.g}, ${this.b})`)
+        this.palco.appendChild(div)
+    }
+
 
     controlar = () => {
 
