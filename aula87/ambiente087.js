4005bf6ac87450cae7f515b4fc5b870f744dc429
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-08 12:31:32 -0300

    aula87 -  redirecionamento e histórico da página

---
+++ aula87/ambiente087.js
@@ -0,0 +1,22 @@
+const url = document.querySelector('#url')
+const btn_url = document.querySelector('#btn_url')
+
+btn_url.addEventListener('click', (evt) => {
+    // window.location = 'https://www.google.com.br'
+
+    // window.location.assign('https://www.google.com.br')
+
+    // window.location.replace('https://www.google.com.br')
+
+    // window.location.reload()
+
+    // window.history.back()
+
+    // window.history.forward()
+
+    // window.history.go(1)
+
+    // console.log(window.history.length)
+
+    // window.location = url.value
+})
