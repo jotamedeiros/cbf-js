a50151e54ef41143636182a9e20772214a55c1e0
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-11 16:39:31 -0300

    aula94 - trabalhando com modularização - pt. 3

---
+++ aula94/ambiente094.js
@@ -0,0 +1,8 @@
+import getTodosCursos from './cursos3.js'
+// import { cursos as c, getCurso as gtc } from './cursos3.js'
+import * as m_cursos from './cursos3.js'
+
+
+console.log(m_cursos.cursos)
+console.log(m_cursos.getCurso(1))
+console.log(m_cursos.default())
---
+++ aula94/cursos3.js
@@ -0,0 +1,19 @@
+const cursos = ['JavaScript', 'HTML', 'CSS', 'Arduino', 'Raspberry', 'C++', 'Python', 'Java', 'C#']
+
+// const getTodosCursos = () => {
+//     return cursos
+// }
+
+// export default getTodosCursos
+
+
+export default function getTodosCursos() {
+    return cursos
+}
+
+function getCurso(i_curso) {
+    return cursos[i_curso]
+}
+
+// export default getTodosCursos
+export {cursos, getCurso}
