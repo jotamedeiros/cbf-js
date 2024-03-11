2b51fcb2b5e9c38a7d8fd9f1e32d7cc29536771b
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-11 18:33:45 -0300

    aula96 - criação do contato de acordo c/ array

--- aula96/contatos.js
+++ aula96/contatos.js
@@ -16,18 +16,23 @@ let contato = {
             email: novoContato.email
         }
         contatos.push(cont)
-        const div = document.createElement('div')
-        div.setAttribute('class', 'contato')
-        const p_nome = document.createElement('p')
-        p_nome.innerHTML = novoContato.nome
-        const p_telefone = document.createElement('p')
-        p_telefone.innerHTML = novoContato.telefone
-        const p_email = document.createElement('p')
-        p_email.innerHTML = novoContato.email
-        div.appendChild(p_nome)
-        div.appendChild(p_telefone)
-        div.appendChild(p_email)
-        destinoDOM.appendChild(div)
+
+        destinoDOM.innerHTML = ''
+
+        contatos.forEach((el) => {
+            const div = document.createElement('div')
+            div.setAttribute('class', 'contato')
+            const p_nome = document.createElement('p')
+            p_nome.innerHTML = el.nome
+            const p_telefone = document.createElement('p')
+            p_telefone.innerHTML = el.telefone
+            const p_email = document.createElement('p')
+            p_email.innerHTML = el.email
+            div.appendChild(p_nome)
+            div.appendChild(p_telefone)
+            div.appendChild(p_email)
+            destinoDOM.appendChild(div)
+        })
     }
 }
 
