e8b489f8eb3886d62124a805b2c15a2031c8b751
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-13 15:30:06 -0300

    aula102 - funções p/ manipulação de string - pt.2

---
+++ aula102/ambiente102.js
@@ -0,0 +1,27 @@
+let nome = new String('Bruno Pinho Campos')
+let nome2 = new String('Bruno Pinho Campos')
+let canal = new String('CFB Cursos')
+
+
+
+// console.log(nome.localCompare(nome2))
+// console.log(nome.replace('o', 'a'))
+// console.log(nome.search('Pinho'))
+// console.log(nome.slice(6, 11))
+
+// let sobrenome = nome.slice(6, 11)
+// console.log(sobrenome)
+
+// let arr_nome = nome.split(' ')
+// console.log(arr_nome)
+
+// let soma = ''
+
+// for (let i = 0; i <= arr_nome.length - 1; i++) {
+//     soma += arr_nome[i]
+// }
+
+// console.log(soma)
+
+// let arr_canal = canal.split('C')
+// console.log(arr_canal)
