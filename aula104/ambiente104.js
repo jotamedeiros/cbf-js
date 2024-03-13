0e8b37821ac40074df5034e8aa54a7471ad81362
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-13 17:14:14 -0300

    aula104 - funções p/ manipulação de string - pt. 4

---
+++ aula104/ambiente104.js
@@ -0,0 +1,52 @@
+let nome = new String('Bruno Pinho Campos')
+
+/* .startsWith()
+console.log(nome.startsWith(''))
+console.log(nome.startsWith('B'))
+console.log(nome.startsWith('Bruno Pinho Campos'))
+console.log(nome.startsWith('Pinho'))
+*/
+
+
+/* .endsWith()
+console.log(nome.endsWith(''))
+console.log(nome.endsWith('s'))
+console.log(nome.endsWith('pos'))
+console.log(nome.endsWith('Campos'))
+console.log(nome.endsWith('o Campos'))
+console.log(nome.endsWith('Pinho Campos'))
+console.log(nome.endsWith('PinhoCampos'))
+*/
+
+
+/* .includes()
+console.log(nome.includes('o '))
+console.log(nome.includes('B'))
+console.log(nome.includes('Pinho'))
+console.log(nome.includes('Cam'))
+console.log(nome.includes('mpos'))
+console.log(nome.includes('bruno'))
+*/
+
+
+/* .repeat()
+console.log(nome.repeat(3))
+console.log(nome.valueOf)
+*/
+
+
+/* String.fromCodePoint()
+console.log(nome.charCodeAt(0))
+console.log(nome.charCodeAt(1))
+console.log(nome.charCodeAt(2))
+console.log(nome.charCodeAt(3))
+console.log(nome.charCodeAt(4))
+
+console.log(String.fromCodePoint(66))
+console.log(String.fromCodePoint(114))
+console.log(String.fromCodePoint(117))
+console.log(String.fromCodePoint(110))
+console.log(String.fromCodePoint(111))
+
+console.log(String.fromCodePoint(66, 114, 117, 110, 111))
+*/
