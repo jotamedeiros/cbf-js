79344264f23aba7c67d372146a8406f3b1a1abf0
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-13 14:18:29 -0300

    aula101 - funções p/ manipulação de strings - pt.1

---
+++ aula101/ambiente101.js
@@ -0,0 +1,17 @@
+let nome = new String('João')
+let canal = new String('CFB Cursos')
+let tanga = new String('Tangamandapio')
+
+// console.log(nome)
+// console.log(typeof(nome))
+
+// console.log(nome.charAt(2))
+// console.log(nome.charAt(0))
+
+// console.log(nome.charCodeAt(2))
+
+// console.log(nome.concat(canal))
+
+// console.log(nome.indexOf('oã'))
+
+console.log(tanga.lastIndexOf('a'))
