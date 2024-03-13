a86e8f7f3ef4745ddce018b34db0f788c17e436d
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-13 16:27:12 -0300

    aula103 - funções p/ manipulação de string - pt. 3

---
+++ aula103/ambiente103.js
@@ -0,0 +1,74 @@
+let nome = new String('Bruno Pinho Campos')
+let nome2 = new String('Bruno Pinho Campos')
+let canal = new String('CFB Cursos')
+
+
+
+// console.log(nome.localCompare(nome2))    // .localCompare()
+
+// console.log(nome.replace('o', 'a'))  // .replace()
+
+// console.log(nome.search('Pinho'))    // .search()
+
+/* .slice()
+console.log(nome.slice(6, 11))
+
+let sobrenome = nome.slice(6, 11)
+console.log(sobrenome)
+*/
+
+
+/* .split()
+let arr_nome = nome.split(' ')
+console.log(arr_nome)
+
+// let soma = ''
+// for (let i = 0; i <= arr_nome.length - 1; i++) {
+//     soma += arr_nome[i]
+// }
+// console.log(soma)
+*/
+
+
+/* .split()
+let arr_canal = canal.split('C')
+console.log(arr_canal)
+*/
+
+
+/* .substring()
+let parte1 = nome.substring(0, 5)
+let sobrenome = nome.substring(6, 11)
+let ultNome = nome.substr(12, 6)
+
+
+console.log(parte1)
+console.log(sobrenome)
+console.log(ultNome)
+*/
+
+
+/* .toUpperCase() & .toLowerCase()
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
+*/
+
+
+/* .valueOf()
+console.log(canal)
+console.log(canal.valueOf())
+*/
+
+
+/* .typeOf()
+let num = 10
+console.log(typeof(num))
+*/
+
+
+/* .toString()
+let num = 10
+console.log(typeof(num.toString()))
+*/
+
+
