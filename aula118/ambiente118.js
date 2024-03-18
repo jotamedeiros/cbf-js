cf494589b116cd7423805f09f9d843caadb42e7e
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-18 17:36:34 -0300

    aula118 - ponto e vírgula obrigatório - pt. 2

---
+++ aula118/ambiente118.js
@@ -0,0 +1,53 @@
+// Exemplo de [;] em um loop
+
+// let i = null
+
+// for (i = 0; i < 10; i++);
+
+// console.log(i)
+
+
+// Exemplo de [;] em declaração de variável
+
+// let i = null; i = 0
+
+
+// Exemplo de [;] quando a próxima linha inicia com [] ou ()
+
+// let n1 = [1, 2]
+// let n2 = [3, 4]
+// [n1, n2].map((el) => {
+//     console.log(el)
+// })
+
+// let canal = 'CFB Cursos';
+// ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't'].forEach((el) => {
+//     console.log(el)
+// })
+
+
+// let n = null
+// (n = 10)
+// console.log(n)
+
+
+// Exemplo de [;] em casos de desestruturação de obj/arrays
+
+// let numeros = [10, 20, 30];
+// [n1, n2, n3] = numeros
+
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
+
+// let num = {
+//     n1: 10,
+//     n2: 20,
+//     n3: 30
+// }
+
+// let {n1, n2, n3} = num
+
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
