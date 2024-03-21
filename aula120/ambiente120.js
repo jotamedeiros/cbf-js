a2587814ca5aae4d281039e2f6d6b93f6c4a643e
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-21 15:50:38 -0300

    aula120 - validação c/ 'setCustomValidity()'

---
+++ aula120/ambiente120.js
@@ -0,0 +1,54 @@
+const f_nome = document.querySelector('#f_nome')
+const f_nota = document.querySelector('#f_nota')
+const f_msg = document.querySelector('#f_msg')
+
+document.querySelector('#btn_validar').addEventListener('click', (evt) => {
+    let estadoValidacao = f_nota.validity
+
+    if (estadoValidacao.valueMissing) {
+        f_nota.setCustomValidity('O campo "Nota" é obrigatório!')
+    } else if (estadoValidacao.rangeOverflow) {
+        f_nota.setCustomValidity('Nossa, que nota alta você digitou!')
+    } else if (estadoValidacao.rangeUnderflow) {
+        f_nota.setCustomValidity('Eita, que nota baixa você digitou!')
+    }
+    f_nota.reportValidity()
+    // f_msg.innerHTML = f_nota.validationMessage
+    evt.preventDefault()
+})
+
+
+/* Métodos de validação do DOM
+
+checkValidity()
+setCustomValidity()
+*/
+
+
+/* Propriedade de validação do DOM
+
+validity
+validationMessage
+*/
+
+
+/* Propriedades de validação
+
+customError: true, se uma mensagem de validação personalizada for definida.
+
+patternMismatch: true, se o valor de um elemento não corresponder ao seu atributo padrão.
+
+rangeOverflow: true, se o valor de um elemento for maior que seu atributo max.
+
+rangeUnderflow: true, se o valor de um elemento for menor que seu atributo min.
+
+stepMismatch: true, se o valor de um elemento for inválido por seu atributo step.
+
+tooLong: true, se o valor de um elemento exceder seu atributo 'maxLength'.
+
+typeMismatch: true, se o valor de um elemento for inválido por seu atributo 'type'.
+
+valueMissing; true, se um elemento (com um atributo obrigatório) não tiver valor.
+
+valid: true, se o valor de um elemento for válido.
+*/
