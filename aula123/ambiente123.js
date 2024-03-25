c77637f5b96e81ff67a1af4168a1f3f5ffe835d6
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-25 17:35:57 -0300

    aula123 - manipulando e automatizando return da API

---
+++ aula123/ambiente123.js
@@ -0,0 +1,22 @@
+const p_temp = document.querySelector('#p_temp')
+const p_nivel = document.querySelector('#p_nivel')
+const p_pressao = document.querySelector('#p_pressao')
+const btn_fetch = document.querySelector('#btn_fetch')
+
+const obterDados = () => {
+    const endpoint = 'https://2bdd268f-1da7-4467-b1ab-7b6893779373-00-a6aleaoqj3vm.worf.replit.dev/'
+
+    fetch(endpoint)
+
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        p_temp.innerHTML = `Temperatura: ${res.temperatura}`
+        p_nivel.innerHTML = `Nível: ${res.nivel}`
+        p_pressao.innerHTML = `Pressão: ${res.pressao}`
+    })
+}
+
+btn_fetch.addEventListener('click', (evt) => {
+    const intervaloId = setInterval(obterDados, 3000)
+})
