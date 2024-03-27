4b3e666c55de6e1deb5ada39e5adb7596136d91f
João Guilherme Medeiros <devjoaogui@gmail.com>
2024-03-27 13:18:05 -0300

    aula136 - criei formatação inicial da message box

--- aula135/scripts/boxmsg.js
+++ aula135/scripts/boxmsg.js
@@ -1,8 +1,15 @@
 const head = document.head
 
 const estilo = '<link rel="stylesheet" href="./styles/boxmsg.css"></link>'
+head.innerHTML += estilo
 
 class Boxmsg {
+    titulo = null
+    texto = null
+    cor = null
+    destino = null
+    divmsg = null
+
     constructor(config) {
         this.titulo = config.titulo
         this.texto = config.texto
@@ -11,7 +18,22 @@ class Boxmsg {
     }
 
     mostrar = () => {
-        console.log(this.texto)
+        this.divmsg = document.createElement('div')
+        this.divmsg.setAttribute('id', 'divmsg')
+        this.divmsg.setAttribute('class', 'divmsg')
+        this.destino.prepend(this.divmsg)
+
+        const areaBoxmsg = document.createElement('div')
+        areaBoxmsg.setAttribute('id', 'areaBoxmsg')
+        areaBoxmsg.setAttribute('class', 'areaBoxmsg')
+        this.divmsg.appendChild(areaBoxmsg)
+
+        const tituloBoxmsg = document.createElement('div')
+        tituloBoxmsg.setAttribute('id', 'tituloBoxmsg')
+        tituloBoxmsg.setAttribute('class', 'tituloBoxmsg')
+        tituloBoxmsg.style.backgroundColor = `${this.cor}`
+        tituloBoxmsg.innerHTML = this.titulo
+        areaBoxmsg.appendChild(tituloBoxmsg)
     }
 
     ocultar = () => {
